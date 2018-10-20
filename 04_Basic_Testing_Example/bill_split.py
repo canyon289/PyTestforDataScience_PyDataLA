61e839707a733c416f405c1dbb6c9e4225c610ae
canyon <ravinsdrive@gmail.com>
2018-10-20 09:45:06 -0700

    Fix bug in 04

--- 04_Basic_Testing_Example/bill_split.py
+++ 04_Basic_Testing_Example/bill_split.py
@@ -2,10 +2,12 @@
 Quick function to split bills
 """
 
+
 def split_bill(cost):
     split_cost = cost / 2
     return split_cost
 
 
-# Split a 5 dinner cost 
-split_bill(5) 
+if __name__ == "__main___":
+    # Split a 5 dinner cost
+    split_bill(5)
--- 04_Basic_Testing_Example/test_bill_split.py
+++ 04_Basic_Testing_Example/test_bill_split.py
@@ -3,10 +3,11 @@ import bill_split
 # pytest autodiscovers tests through module, class, function names
 # https://pytest.readthedocs.io/en/reorganize-docs/new-docs/user/naming_conventions.html
 
+
 def test_bill_splitting():
     """Test that bill splitting works"""
     bill_cost = 5
-    split = bill_split.split_bill(5)
+    split = bill_split.split_bill(bill_cost)
 
     # Check result with assert statment and conditional
     assert split == 2.5
