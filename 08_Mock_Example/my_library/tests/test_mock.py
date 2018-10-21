e2eec236a41865007532f9041e5b9fe4cfc97214
canyon <ravinsdrive@gmail.com>
2018-10-21 15:33:20 -0700

    Fix mistake in example 8

--- 08_Mock_Example/my_library/tests/test_mock.py
+++ 08_Mock_Example/my_library/tests/test_mock.py
@@ -4,13 +4,13 @@ from .. import my_module
 
 def test_big():
     dataframe = Mock()
-    Mock.shape = (2000000, 1)
+    dataframe.shape = (2000000, 1)
 
     assert my_module.is_dataframe_big(dataframe) is True
 
 
 def test_not_big():
     dataframe = Mock()
-    Mock.shape = (100, 1)
+    dataframe.shape = (100, 1)
 
     assert my_module.is_dataframe_big(dataframe) is False
